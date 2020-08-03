3b251d6515845469072f84a6f92e31a27ee1b334
ChenLeYao <18814515448@163.com>
2020-08-03 11:33:49 +0800

    first commit

---
+++ src/main/index.js
@@ -0,0 +1,4 @@
+import test  from '../image/test.jpg';
+let image = new Image();
+image.src = test;
+document.body.appendChild(image);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+    entry : './src/main/index.js',
+    output : {
+        filename: "[name].js",
+        path: path.resolve(__dirname , "build"),
+        publicPath: "./" ,
+        chunkFilename: "[name]_[contentHash].js"
+    },
+    mode : 'development',
+    module: {
+        rules: [
+            {
+                test :/\.css$/ ,
+                use : ['css-loader' ,'style-loader']
+            },
+            {
+                test : /\.(png|jpg|jpeg|gif)$/,
+                use : {
+                    loader:'file-loader',
+                    options: {
+                        outputPath : 'image'
+                    }
+                }
+            },
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title : 'webpack配置模板',
+            template: "./src/view/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
+
+
+
